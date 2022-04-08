a31b994e48abdda09913d713232493bfd32b920c
phamminhquoccodinggithub <74046383+phamminhquoccodinggithub@users.noreply.github.com>
2022-04-08 21:23:03 +0700

    Add static resources

--- src/main/java/com/mycompany/configs/WebAppContextConfig.java
+++ src/main/java/com/mycompany/configs/WebAppContextConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -30,7 +31,11 @@ public class WebAppContextConfig implements WebMvcConfigurer {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/resources/images/");
+    }
     @Bean
     public InternalResourceViewResolver getInternalResourceViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -45,4 +50,5 @@ public class WebAppContextConfig implements WebMvcConfigurer {
         msg.setBasenames("messages");
         return msg;
     }
+    
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -4,6 +4,7 @@
     Author     : Quoc
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <!DOCTYPE html>
@@ -11,10 +12,12 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Homepage</title>
+        <link href="<c:url value="/css/style.css"/>" rel="stylesheet"/>
     </head>
     <body>
         <h1><spring:message code="page.title"/></h1>
         <h1>Hello ${name}!</h1>
         <h3><spring:message code="page.welcome"/></h3>
+        <h3><img src="<c:url value="/img/bus.jpg"/>"</h3>
     </body>
 </html>
